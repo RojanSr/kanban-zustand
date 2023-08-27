d63494355f178566a2b030948d275ecd1f9ddb8e
RojanSr <rairojan1116@gmail.com>
2023-08-27 17:43:02 +0545

    change grayDark color

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -2,7 +2,7 @@ export const zusColor = {
   gray: "#49525B",
   // gray: "#333333",
   // grayDark: "#222222",
-  grayDark: "#2D3339",
+  grayDark: "hsl(217, 19%, 24%)",
   grayLight: "#cccccc",
   planned: "#B2A198",
   ongoing: "#96c6ca",
