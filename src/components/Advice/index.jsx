c3e72d2809dabfa78efed81a7d4239fe167ac3a5
RojanSr <rairojan1116@gmail.com>
2023-08-28 11:20:29 +0545

    add: error handling for api fetch

--- src/components/Advice/index.jsx
+++ src/components/Advice/index.jsx
@@ -22,7 +22,7 @@ const AdviceCard = () => {
     }
   }
 
-  const { data, isLoading, isFetching, error, refetch } = useQuery({
+  const { data, isLoading, isFetching, isError, refetch } = useQuery({
     queryKey: ["adviceData"],
     queryFn: fetchAdvice,
     staleTime: 60 * 1000, // 1 minute
@@ -30,29 +30,11 @@ const AdviceCard = () => {
     refetchOnWindowFocus: false,
   });
 
-  if (isFetching || isLoading) {
-    return (
-      <Box
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        textAlign="center"
-        bg="hsl(217, 19%, 24%)"
-        borderRadius="12px"
-        py={6}
-        px={4}
-        maxW={{ base: "290px", md: "420px" }}
-        minH="150px"
-        minW={{ base: "270px", md: "380px" }}
-      >
-        <Spinner color="hsl(193, 38%, 86%)" size="lg" />
-      </Box>
-    );
-  }
   return (
     <Box
       display="flex"
       justifyContent="center"
+      alignItems="center"
       textAlign="center"
       flexDirection="column"
       bg="hsl(217, 19%, 24%)"
@@ -63,38 +45,49 @@ const AdviceCard = () => {
       px={4}
       maxW={{ base: "290px", md: "420px" }}
       minW={{ base: "270px", md: "380px" }}
+      minH="150px"
       position="relative"
     >
-      <Text color="hsl(150, 100%, 66%)" fontSize="12px" letterSpacing="1px">
-        {data?.author ? data?.author : "Anonymous"}
-      </Text>
-      <Text
-        as="q"
-        color="hsl(193, 38%, 86%)"
-        fontSize="18px"
-        fontWeight="700"
-        lineHeight="7"
-      >
-        {data?.quote.replace(/[!@#$^*]/g, "")}
-      </Text>
-      <Center
-        position="absolute"
-        bottom="-25px"
-        right="50%"
-        transform="translateX(50%)"
-        bg="hsl(150, 100%, 66%)"
-        color="#fff"
-        w="40px"
-        h="40px"
-        borderRadius="50%"
-        cursor="pointer"
-        _hover={{
-          background: "hsl(150, 100%, 40%)",
-        }}
-        onClick={refetch}
-      >
-        <BsFillDice5Fill color="#000" />
-      </Center>
+      {isFetching || isLoading ? (
+        <Spinner color="hsl(193, 38%, 86%)" size="lg" />
+      ) : isError ? (
+        <Text>
+          Unable to reach. Please try checking you internet connection
+        </Text>
+      ) : (
+        <>
+          <Text color="hsl(150, 100%, 66%)" fontSize="12px" letterSpacing="1px">
+            {data?.author ? data?.author : "Anonymous"}
+          </Text>
+          <Text
+            as="q"
+            color="hsl(193, 38%, 86%)"
+            fontSize="18px"
+            fontWeight="700"
+            lineHeight="7"
+          >
+            {data?.quote.replace(/[!@#$^*]/g, "")}
+          </Text>
+          <Center
+            position="absolute"
+            bottom="-25px"
+            right="50%"
+            transform="translateX(50%)"
+            bg="hsl(150, 100%, 66%)"
+            color="#fff"
+            w="40px"
+            h="40px"
+            borderRadius="50%"
+            cursor="pointer"
+            _hover={{
+              background: "hsl(150, 100%, 40%)",
+            }}
+            onClick={refetch}
+          >
+            <BsFillDice5Fill color="#000" />
+          </Center>
+        </>
+      )}
     </Box>
   );
 };
